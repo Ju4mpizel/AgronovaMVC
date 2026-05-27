303da66e6dc95e93f5dfb8c97f483048aa70dd7d
Ju4mpizel <juampizel@gmail.com>
2026-05-27 11:54:04 -0400

    Correcciones Login y Auth

--- views/login.php
+++ views/login.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../controllers/AuthController.php';
+require_once __DIR__ . '/../controllers/AuthController.php';
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
